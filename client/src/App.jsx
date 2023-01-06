a00ca48e2867643be8add2c1f4d13c6a4cf56f0e
kumarmanishrai <manishkumarrai79@gmail.com>
2023-01-06 23:06:39 +0530

    made a simple ui for the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [note, setNote] = useState('')
+
+  const handleChange = (event) => {
+    setNote(event.target.value)
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="inp-area">
+        <input onChange={handleChange} type="text" id="inp" placeholder='type here..' />
+        <button onClick={''}>Save</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="notes">
+        
+        <div className="note">
+          <div id='text'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores ea, cumque, a, porro velit tempora molestias optio neque dolorum ut laudantium qui facilis iure dicta illum quia deleniti. Cum, vitae! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt eaque repellendus necessitatibus sit officiis fugiat quia a deserunt soluta mollitia nemo nostrum, commodi delectus nesciunt ullam aut, magnam alias modi.
+          </div>
+          <span id='extra'>
+            buttons
+            </span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+
   )
 }
 
